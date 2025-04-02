18983f46eb3b0304c4f55abb6634a97164425b5d
GraceKim527 <lemonherb0323@naver.com>
2025-04-02 10:43:37 +0000

    🔁 Sync from team repo: b0c24c1e6b2fa0d3c0b8f6b1ffd2e9b36fdab310

--- src/libs/instance.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/instance.ts
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  withCredentials: true,
-});
-
-export default instance;
